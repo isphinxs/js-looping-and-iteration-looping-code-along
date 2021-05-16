e08a10e33c793caaa2fff7b2773eb0a414942581
Samantha Ostrowski <isphinxs@hotmail.com>
2021-05-15 21:25:42 -0400

    commit tree 4c555741d04d3ae550574f96aa597d232d2a8d77

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(arrOfNames, eventName) {
+    const messages = [];
+    for (let i = 0; i < arrOfNames.length; i ++) {
+        messages[i] = `Thank you, ${arrOfNames[i]}, for the wonderful ${eventName} gift!`
+    }
+    return messages; 
+}
+
+function countDown(integer) {
+    let countdown = integer;
+    while (countdown >= 0) {
+        console.log(countdown);
+        countdown --;
+    }
+}
